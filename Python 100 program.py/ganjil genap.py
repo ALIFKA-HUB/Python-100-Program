086480df9dfb5959aacb04bf0c8f6370b13d0734
= <=>
2024-11-15 07:37:57 +0700

    lulus

---
+++ Python 100 program.py/ganjil genap.py
@@ -0,0 +1,8 @@
+angka = int(input("Masukkan sebuah angka = "))
+
+if angka % 2 == 0:
+    print("Angka tersebut adalah genap.")
+elif angka % 2 == 1:
+    print("Angka tersebut adalah ganjil.")
+else:
+    print("bukan berupa angka")
